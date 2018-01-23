a91c120d59ea78c1638c0dd25fd171e167074098
Nabil001 <nabil.lemenuel@gmx.fr>
2018-01-23 23:21:25 +0100

    completed Image class

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -2,7 +2,41 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="image")
+ */
 class Image
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $url;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
 }
