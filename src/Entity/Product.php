e210212231563f3ccdfca50f0d38c7875111fde7
Nabil001 <nabil.lemenuel@gmx.fr>
2018-01-23 23:16:58 +0100

    completed Product class

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,7 +2,95 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="product")
+ */
 class Product
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="decimal", precision=6, scale=2)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(name="taxe_rate", type="decimal", precision=5, scale=2)
+     */
+    private $taxeRate;
+
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name) : void
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription() : string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description) : void
+    {
+        $this->description = $description;
+    }
+
+    public function getPrice() : float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price) : void
+    {
+        $this->price = $price;
+    }
+
+    public function getTaxeRate() : float
+    {
+        return $this->taxeRate;
+    }
+
+    /**
+     * @param float $taxeRate
+     */
+    public function setTaxeRate(float $taxeRate) : void
+    {
+        $this->taxeRate = $taxeRate;
+    }
 }
