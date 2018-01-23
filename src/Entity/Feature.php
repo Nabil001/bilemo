9d2683b5fd9b02225514a28123fd3e6b4a361411
Nabil001 <nabil.lemenuel@gmx.fr>
2018-01-23 23:57:00 +0100

    complted Feature class, stated editing ProductFeature class

--- src/Entity/Feature.php
+++ src/Entity/Feature.php
@@ -2,7 +2,43 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="feature")
+ */
 class Feature
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): Feature
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
--- src/Entity/ProductFeature.php
+++ src/Entity/ProductFeature.php
@@ -2,7 +2,38 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="product_feature")
+ */
 class ProductFeature
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $value;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue(string $value): ProductFeature
+    {
+        $this->value = $value;
+
+        return $this;
+    }
 }
