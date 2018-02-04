c6b120bd8deba1f355c3523a2318f7e2ac0f54ed
Nabil001 <nabil.lemenuel@gmx.fr>
2018-02-04 15:21:45 +0100

    installed security pack and lexik/jwt-authentication-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
